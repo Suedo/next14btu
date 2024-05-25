3ae6f3edfa66dabee740c2292699e95232131033
Somjit Nag <somjitnag192@gmail.com>
2024-05-25 12:50:15 +0530

    basic SSR pages and LINKing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      {/* a normal <a> tag will cause a full reload, Link does not, and is more performant */}
+      <Link href="/properties">show properties</Link>
+    </div>
+  )
 }
 
 export default HomePage
 
-// page.tsx is the default home page in next's file based routing
+// app/page.tsx is the default home page in next's file based routing
---
+++ src/app/properties/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PropertiesPageId = () => {
+  return <div>PropertiesPageId</div>
+}
+
+export default PropertiesPageId
+
+// instead of [id] as foldername, you can do [...id], which is a catch-all
+// meaning /id, /id/123, /id/others/123 any such paths starting with id will be caught
